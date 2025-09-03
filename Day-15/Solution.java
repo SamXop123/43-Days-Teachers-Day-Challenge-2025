337095bab12fb672ee6fa15164501ac373fc0f8a
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-03 16:21:50 +0530

    Update Solution.java

--- Day-15/Solution.java
+++ Day-15/Solution.java
@@ -1,1 +1,33 @@
-
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int count = 0;
+        
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+            boolean isUnique = true;
+            
+            for (int j = 0; j < i; j++) {
+                if (input.charAt(j) == current) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            
+            if (isUnique) {
+                count++;
+            }
+        }
+        
+        if (count % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+        
+        sc.close();
+    }
+}
