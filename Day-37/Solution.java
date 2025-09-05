a505510b5df2327f250499075a40117f68e79bc5
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-05 22:03:33 +0530

    Create Solution.java

---
+++ Day-37/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        String input = sc.next();
+ 
+        if (input.matches(".*[HQ9].*")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+ 
+        sc.close();
+    }
+}
