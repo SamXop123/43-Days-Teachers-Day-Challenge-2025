99b5b4e5fb5b7067ad3aab3011697b33b627e7ba
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-05 22:36:37 +0530

    Create Solution.java

---
+++ Day-41/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+ 
+        int xSum = 0, ySum = 0, zSum = 0;
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+ 
+            xSum += x;
+            ySum += y;
+            zSum += z;
+        }   
+ 
+        if(xSum == 0 &&
+           ySum == 0 &&
+           zSum == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+ 
+        sc.close();
+    }
+}
